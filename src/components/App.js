9a52a8a9e1c208a757de148aea3bc3214e32a9a2
Sahana Rajasekar <shnraj@gmail.com>
2018-02-05 15:38:39 -0800

    Upvote and downvote in the UI, clean up styling

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,9 @@ class App extends Component {
   constructor() {
     super();
 
-    // this.addFish = this.addFish.bind(this);
     this.reloadJokes = this.reloadJokes.bind(this);
+    this.handleUpvote = this.handleUpvote.bind(this);
+    this.handleDownvote = this.handleDownvote.bind(this);
 
     // getinitialState
     this.state = {
@@ -25,6 +26,34 @@ class App extends Component {
     .then(data => this.setState({ jokes: data }));
   }
 
+  updateJokeCount(jokeId, offset) {
+    // take a copy of our state
+    const jokes = {...this.state.jokes};
+    // update or add the new number of fish ordered
+    jokes[jokeId].votes = jokes[jokeId].votes + offset;
+    // update our state
+    this.setState({ jokes });
+  }
+
+  vote(url, jokeId) {
+    fetch(url + '"' + jokeId + '"', {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      }
+    })
+  }
+
+  handleUpvote(jokeId) {
+    this.vote('http://shnraj.pythonanywhere.com/joke/upvote/', jokeId)
+    this.updateJokeCount(jokeId, 1);
+  }
+
+  handleDownvote(jokeId) {
+    this.vote('http://shnraj.pythonanywhere.com/joke/downvote/', jokeId)
+    this.updateJokeCount(jokeId, -1);
+  }
+
   render() {
     return (
       <div className="app">
@@ -39,7 +68,16 @@ class App extends Component {
             {
               Object
                 .keys(this.state.jokes)
-                .map(key => <Joke key={key} id={key} text={this.state.jokes[key].joke} count={this.state.jokes[key].votes} />)
+                .map(key => 
+                  <Joke 
+                    key={key} 
+                    id={key} 
+                    joke={this.state.jokes[key].joke} 
+                    votes={this.state.jokes[key].votes} 
+                    handleUpvote={this.handleUpvote}
+                    handleDownvote={this.handleDownvote}
+                  />
+                )
             }
           </ul>
         </div>
--- src/components/Joke.js
+++ src/components/Joke.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 class Joke extends React.Component {
   render() {
-    const { id, text, count } = this.props;
+    const { id, joke, votes } = this.props;
     const isAvailable = true;
     // const isAvailable = details.status === 'available';
     // const buttonText = isAvailable ? 'Add To Order' : 'Sold Out!';
     return (
       <div className="joke">
         <div className="counter">
-            <button onClick={() => this.props.handleUpvoteClick(id)} disabled={!isAvailable}>+</button>
-            <h2 className="count">{count}</h2>
-            <button onClick={() => this.props.handleDownvoteClick(id)} disabled={!isAvailable}>-</button>
+            <button onClick={() => this.props.handleUpvote(id)} disabled={!isAvailable}>+</button>
+            <h2 className="count">{votes}</h2>
+            <button onClick={() => this.props.handleDownvote(id)} disabled={!isAvailable}>-</button>
         </div>
         <div className="text">
-          <p>{text}</p>
+          <p>{joke}</p>
         </div>
       </div>
     )
